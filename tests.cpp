39bad11adbd7e0ee5a4ede36ca3ad114fc8c3e73
Erlend Larsen <erlend.larsen@ffi.no>
2015-07-13 15:40:22 +0200

    Attempting google test framework

---
+++ tests.cpp
@@ -0,0 +1,36 @@
+//#include "helperfunctions.h"
+
+#include <iostream>
+#include <string>
+#include <cmath> 
+#include <gtest/gtest.h>
+using namespace std; 
+
+
+
+TEST(SQTest, PositiveNos){
+ASSERT_EQ(6,sqrt(36.0));
+}
+
+TEST(SQTest, NegativeNos){
+ASSERT_EQ(-1.0,sqrt(-15.0));
+}
+
+int main (int argc, char **argv) 
+{
+//	Helperfunctions h;
+//	h.TestAllFunctions();
+        string s="Erlend";
+
+
+        cout << "Hello, " << s << '\n' ;
+
+testing::InitGoogleTest(&argc, argv);
+
+
+
+
+return RUN_ALL_TESTS();
+        //return 0; // this return statement isn't necessary 
+}
+
